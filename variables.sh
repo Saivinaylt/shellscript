a5b485307c62ab43db44903b3e0dcd6e9f9da745
Saivinay <saivinay165@pec.edu>
2024-02-08 19:16:32 +0530

    first commit

--- variables.sh
+++ variables.sh
@@ -17,3 +17,15 @@ echo
 echo " ${frined1} he dont want ${frined2} "
 echo 
 echo " ${frined1} just summa fn testing variables concepts ${frined2} "
+
+frined1=$1
+frined2=$2
+
+echo "if you wan to pass the values from the command line "
+echo " ${frined1} and ${frined2} "
+echo
+echo " ${frined1} wants to meet ${frined2} "
+echo
+echo " ${frined1} he dont want ${frined2} "
+echo 
+echo " ${frined1} just summa fn testing variables concepts ${frined2} "
