d7926e4a8fa4e39c2e5caa1b33817a05726c2fef
Saivinay <saivinay165@pec.edu>
2024-02-08 19:25:25 +0530

    first commit

--- variables.sh
+++ variables.sh
@@ -30,8 +30,8 @@ echo " ${frined1} he dont want ${frined2} "
 echo 
 echo " ${frined1} just summa fn testing variables concepts ${frined2} "
 
-echo  "enter username:"
+echo -n "enter username:"
 
-echo  "enter passwd:"
+echo -n "enter passwd:"
 
 echo
