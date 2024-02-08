e38a5ce28dbdbdfbc7812830c3773350eb4fa863
Saivinay <saivinay165@pec.edu>
2024-02-08 19:09:13 +0530

    first commit

--- variables.sh
+++ variables.sh
@@ -11,3 +11,9 @@ frined1=Ramesh
 frined2=Suresh
 
 echo " ${frined1} and ${frined2} "
+echo
+echo " ${frined1} wants to meet ${frined2} "
+echo
+echo " ${frined1} he dont want ${frined2} "
+echo 
+echo " ${frined1} just summa fn testing variables concepts ${frined2} "
