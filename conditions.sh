afb2524d1c605c388a539b743aea15108fcb963d
Saivinay <saivinay165@pec.edu>
2024-02-09 18:43:40 +0530

    first commit

--- conditions.sh
+++ conditions.sh
@@ -37,3 +37,29 @@ else
 fi
 
 ls -ltr
+
+
+
+sudo apt update
+
+if [ $? -ne 0 ]; then
+    echo " updating the apt repo failure"
+    exit 1
+else 
+    echo "updating the apt repo Sucesss"
+fi 
+
+# sudo apt install fontconfig openjdk-17-jre
+
+# java -version
+
+# sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+#   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+# echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+#   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+#   /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# sudo apt-get update
+
+# sudo apt-get install jenkins
