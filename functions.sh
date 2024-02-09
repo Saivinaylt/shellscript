238870b574d9aa2e24daaf47cdc3fe3f2c18a99a
Saivinay <saivinay165@pec.edu>
2024-02-09 19:22:09 +0530

    first commit

--- functions.sh
+++ functions.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-jenkins(){
-   sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.ke
+# jenkins(){
+#    sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+#   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.ke
 
-  echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+#   echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+#   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+#   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-  sudo apt-get update
+#   sudo apt-get update
 
-  sudo apt-get install jenkins 
+#   sudo apt-get install jenkins 
 
-  systemctl status jenkins
+#   systemctl status jenkins
+# }
+
+# jenkins
+
+VALIDATE(){
+
+if [ $? -ne 0 ]; then
+    echo " updating the apt repo failure $1"
+    exit 1
+else 
+    echo "updating the apt repo Sucesss $1"
+fi
+
 }
 
-jenkins
+
+
+VALIDATE $1
+
