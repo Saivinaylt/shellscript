8f069d1fe45d3d9a318702717070fa97686e0b89
Saivinay <saivinay165@pec.edu>
2024-02-09 18:51:21 +0530

    first commit

--- conditions.sh
+++ conditions.sh
@@ -61,7 +61,7 @@ fi
 java -version
 
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key > jenlins.logs
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key 
 
 if [ $? -ne 0 ]; then
     echo " failure"
@@ -81,7 +81,7 @@ fi
 
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null > jenlins.logs
+  /etc/apt/sources.list.d/jenkins.list > /dev/null 
 
 if [ $? -ne 0 ]; then
     echo " failure"
