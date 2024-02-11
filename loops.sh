905b4759ed0fc4e996bc253f1e8fe2b4c9eb66b4
Saivinay <saivinay165@pec.edu>
2024-02-11 12:27:31 +0530

    first commit

--- loops.sh
+++ loops.sh
@@ -15,16 +15,23 @@ a=`expr $a + 1`
 
 done
 
-# for 
+# # for 
+# for a in 1 2 3 4 5 6 7 8 9 10
+# do
+# if [ $a == 5 ]
+# then
+# break
+# fi
+# echo "print th iteration: $a"
+# done
+
 for a in 1 2 3 4 5 6 7 8 9 10
 do
-
 if [ $a == 5 ]
 then
 break
 fi
-
-echo "print th iteration: $a"
+echo “Iteration no $a”
 done
 
 
