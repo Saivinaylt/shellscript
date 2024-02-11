6349c8f3617b0963cfcf7af9152f23d8390bda1e
Saivinay <saivinay165@pec.edu>
2024-02-11 12:43:09 +0530

    first commit

--- loops.sh
+++ loops.sh
@@ -15,15 +15,15 @@
 
 # done
 
-# # for 
-# for a in 1 2 3 4 5 6 7 8 9 10
-# do
-# if [ $a == 5 ]
-# then
-# break
-# fi
-# echo "print th iteration: $a"
-# done
+# for 
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+if [ $i -eq 5 ]
+then
+break
+fi
+echo "print th iteration: $i"
+done
 
 
 for a in 1 2 3 4 5 6 7 8 9 10
@@ -32,7 +32,7 @@ do
 # if a = 5 then continue the loop and
 # don’t move to line 8
 
-if [ $a -lt 5 ]
+if [ $a -eq 5 ]
 then
 continue
 fi
