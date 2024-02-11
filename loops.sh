71ecbd3e7961d43da654166323fb082006492fc0
Saivinay <saivinay165@pec.edu>
2024-02-11 11:54:07 +0530

    first commit

--- loops.sh
+++ loops.sh
@@ -6,10 +6,10 @@ a=0
 
 #Iterate the loop until a less than 10
 
-while [ $a -lt 10 ]
+while [ $a -lt 50 ]
 do
 # Print the values
 echo $a
 # increment the value
-a=`expr $a + 1`
+a=`expr $a + 2`
 done
