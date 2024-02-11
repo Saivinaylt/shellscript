6579144c7da97295c24452ba5d786e9622ed8f3a
Saivinay <saivinay165@pec.edu>
2024-02-11 12:14:09 +0530

    first commit

--- loops.sh
+++ loops.sh
@@ -11,10 +11,24 @@ while [ $a -lt 20 ]
 do 
 echo $a
 
-a=`expr $a+1`
+a=`expr $a + 1`
 
 done
 
 
 
 echo done
+
+# a=0
+
+# # lt is less than operator
+
+# #Iterate the loop until a less than 10
+
+# while [ $a -lt 10 ]
+# do
+# # Print the values
+# echo $a
+# # increment the value
+# a=`expr $a + 1`
+# done
